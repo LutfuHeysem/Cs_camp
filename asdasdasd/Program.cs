7d655e48a9e5afcfd745807312d1fbe7a800c243
Lutfu Heysem KIZILOGLU <lutfukzlgl@outlook.com>
2021-01-21 00:39:59 +0300

    21 Jan 2021

---
+++ asdasdasd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace asdasdasd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
